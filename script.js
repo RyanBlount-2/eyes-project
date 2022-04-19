6b1e3c69d69bef54c49297f8e60628a48f3c9578
Ryan Blount <89652477+RyanBlount-2@users.noreply.github.com>
2022-04-19 09:08:53 -0500

    Add Files Via Upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const pupils = document.getElementsByClassName('pupil');
+
+document.onmousemove = (event) => {
+   const x = (event.clientX * 100) / window.innerWidth + '%';
+   const y = (event.clientY * 100) / window.innerHeight + '%';
+   for (let i = 0; i < 2; i++) {
+      pupils[i].style.left = x;
+      pupils[i].style.top = y;
+      pupils[i].transform = 'translate(-' + x + ', -' + y + ')';
+   }
+}
